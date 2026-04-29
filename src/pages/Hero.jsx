3006b932e283f6ca6007be63f4e723e8acc8504a
Hafidz Fauzi <masozi395@gmail.com>
2026-04-29 16:56:38 +0700

    ✨ feat(Home.jsx): Home Section
    Pembuatan base home setion

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <section className="h-screen flex items-center justify-center bg-black text-white">
+      <div data-aos="fade-up" className="text-center">
+        <h1 className="text-5xl font-bold">
+          Hafidz Fauzi
+        </h1>
+
+        <p data-aos="fade-up" data-aos-delay="200" className="mt-4 text-gray-400">
+          Cybersecurity Engineering Student
+        </p>
+      </div>
+    </section>
+  )
+}
