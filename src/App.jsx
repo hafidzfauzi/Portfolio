2a49f0afd5d3878fc8d2910fdbf33c2189788934
Hafidz Fauzi <masozi395@gmail.com>
2026-04-29 17:00:59 +0700

    🦄 refactor(Import Page dan Components): Pemanggilan section
    Pemanggilan section pada APP.jsx sebagai tampilan

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Hero from './pages/Hero'
+import About from './pages/About'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <>
-      <section className="h-screen flex items-center justify-center bg-black text-white">
-        <div className="text-center">
-          <h1 className="text-5xl font-bold">Hafidz Fauzi</h1>
-          <p className="mt-4 text-gray-400">
-            Cybersecurity Engineering Student
-          </p>
-        </div>
-      </section>
+      <Navbar />
+      <Hero />
+      <About />
     </>
   )
 }
