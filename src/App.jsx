eaf480fa4bc9d32d43bc15015336391cc07266a0
Hafidz Fauzi <masozi395@gmail.com>
2026-05-25 02:30:12 +0700

    style: update portfolio layout and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,25 @@ import './App.css'
 import Hero from './pages/Hero'
 import About from './pages/About'
 import Navbar from './components/Navbar'
+import Skills from './pages/Skills'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 import Footer from './components/Footer'
+import AnimatedBackground from './components/AnimatedBackground'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
     <>
+      <AnimatedBackground />
       <Navbar />
       <Hero />
       <About />
+      <Skills />
       <Projects />
       <Contact />
       <Footer />
+      <ScrollToTop />
     </>
   )
 }
